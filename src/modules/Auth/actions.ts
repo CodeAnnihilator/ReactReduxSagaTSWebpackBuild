84f51523de10de482b35d919ce709d74d8aa791b
Разводов Игорь <razvodov_iv@km-union.ru>
2019-06-07 08:40:38 +0300

    added verifyToken saga
    minor changes
    
    auth flow update

--- src/modules/Auth/actions.ts
+++ src/modules/Auth/actions.ts
@@ -1,4 +1,4 @@
-import {createStandardAction} from 'typesafe-actions';
+import {action, createCustomAction, createAction, createStandardAction} from 'typesafe-actions';
 
 import {authActionTypes} from './constants';
 
@@ -6,7 +6,24 @@ import {authActionTypes} from './constants';
 // 	createStandardAction(authActionTypes.SEND_AUTH_DATA)
 // 		<any>();
 
+// export const sendAuthData = (login: string, pass: string) => ({
+// 	type: authActionTypes.SEND_AUTH_DATA,
+// 	login,
+// 	pass,
+// });
+export const sendAuthData = createCustomAction(authActionTypes.SEND_AUTH_DATA, type =>
+	(login: string, pass: string) => ({
+		type, login, pass,
+	}));
+
 export const startAuthRequest = createStandardAction(authActionTypes.START_AUTH_REQUEST)();
 export const stopAuthRequest = createStandardAction(authActionTypes.STOP_AUTH_REQUEST)();
-export const login = createStandardAction(authActionTypes.LOGIN)();
+export const login = createCustomAction(authActionTypes.LOGIN, type =>
+	(exp: number, userId: string, caption: string, permissions: string[]) => ({
+		type, exp, userId, caption, permissions,
+	}),
+);
 export const logout = createStandardAction(authActionTypes.LOGOUT)();
+export const startTokenRefresh = createStandardAction(authActionTypes.START_TOKEN_REFRESH)();
+export const stopTokenRefresh = createStandardAction(authActionTypes.STOP_TOKEN_REFRESH)();
+export const verifyToken = createStandardAction(authActionTypes.VERIFY_TOKEN)();
